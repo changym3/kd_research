62e001a0cbd989408c5e6200cdf730eb4a2b6e0f
changym3 <554336176@qq.com>
2022-02-21 21:51:00 +0800

    [feat] MLP支持k-hop augmented features

--- kd/trainer/MLP.py
+++ kd/trainer/MLP.py
@@ -1,6 +1,6 @@
 import torch
 from torch_geometric.nn.models import MLP
-
+import torch_geometric.transforms as T
 from kd.utils.evaluator import Evaluator
 from kd.utils.logger import Logger
 
@@ -8,16 +8,35 @@ class MLPTrainer:
     def __init__(self, cfg, dataset, device):
         self.cfg = cfg
         self.dataset = dataset
-        self.data = dataset[0].to(device)
+        self.data = self.preprocess_data(cfg, dataset[0].to(device))
         self.device = device
         self.model = self.build_model(cfg).to(device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=cfg.trainer.lr, weight_decay=cfg.trainer.weight_decay)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.evaluator = Evaluator()
         self.logger = Logger()
+    
+    def preprocess_data(self, cfg, data):
+        aug_k = cfg.model.aug.k
+        aug_combine = cfg.model.aug.combine
+
+        transform = T.SIGN(aug_k)
+        data = transform(data)
+        x_list = [data.x]
+        for i in range(1, aug_k+1):
+            x_list.append(getattr(data, f'x{i}'))
+            delattr(data, f'x{i}')
+        if aug_combine == 'cat':
+            data.x = torch.cat(x_list, dim=-1)
+        return data
 
     def build_model(self, cfg):
-        num_features = cfg.dataset.num_features
+        aug_k = cfg.model.aug.k
+        aug_combine = cfg.model.aug.combine
+        if aug_combine == 'cat':
+            num_features = cfg.dataset.num_features * (aug_k + 1)
+        else:
+            num_features = cfg.dataset.num_features
         num_hiddens = cfg.model.num_hiddens
         num_layers = cfg.model.num_layers
         num_classes = cfg.dataset.num_classes
